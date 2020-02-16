a97cbe53e6052a0b7bfa39b2e2025f6abd546206
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-16 20:33:15 +0900

    remove: remove context once

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useUserContext } from '../lib/context/UserContext';
 
 const Login: React.FC = () => {
-  const { user } = useUserContext();
-  // eslint-disable-next-line no-console
-  console.log(user);
-  return <div>{user && <p>user</p>}</div>;
+  return (
+    <div>
+      <p>hogehgoe</p>
+    </div>
+  );
 };
 
 export default Login;
