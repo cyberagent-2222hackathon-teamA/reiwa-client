6bb32dfd16ee49e4e0992cc00b8f8c7167b6e3a3
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-16 16:01:11 +0900

    feat: add getTwitterURL function

--- src/components/template/MenuNavi.tsx
+++ src/components/template/MenuNavi.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getTwitterURL } from '../../lib/api/auth';
 
 export const MenuNavi: React.FC = () => {
+  const getURL = async () => {
+    const { res } = await getTwitterURL();
+    if (res) {
+      const windowLogin = window.open(`${res.url}`);
+      // eslint-disable-next-line no-console
+      console.log({ windowLogin });
+    }
+  };
+
   return (
     <div>
-      <p>
-        <Link to={'/login'}>login</Link>
-      </p>
+      <button onClick={getURL}>login</button>
       <p>
         <Link to={'/setting'}>setting</Link>
       </p>
