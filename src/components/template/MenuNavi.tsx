48f58fdd32b1aefd165f8a18f817ad9f14bcc377
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-17 12:28:43 +0900

    feat: add cookie register

--- src/components/template/MenuNavi.tsx
+++ src/components/template/MenuNavi.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import { getTwitterURL } from '../../lib/api/auth';
 
 export const MenuNavi: React.FC = () => {
+  const [cookies, removeCookie] = useCookies(['user']);
+  // eslint-disable-next-line no-console
+  console.log(cookies);
+
   const getURL = async () => {
     const { res } = await getTwitterURL();
     if (res) {
@@ -10,9 +15,13 @@ export const MenuNavi: React.FC = () => {
     }
   };
 
+  const logOut = async () => {
+    removeCookie('user', '');
+  };
+
   return (
     <div>
-      <button onClick={getURL}>login</button>
+      {cookies['user'] ? <button onClick={logOut}>logOut</button> : <button onClick={getURL}>logIn</button>}
       <p>
         <Link to={'/setting'}>setting</Link>
       </p>
--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useMemo, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { getToken } from '../lib/api/auth';
+import { history } from '../lib/plugins/history';
 
 const Login: React.FC = () => {
+  const [cookies, setCookie] = useCookies(['user']);
+  const query = useMemo(() => {
+    return location.search;
+  }, [location.search]);
+
+  useEffect(() => {
+    // cookieにuserがある場合rootへリダイレクト
+    if (cookies['user']) {
+      history.push('/');
+    }
+    const setToken = async () => {
+      const { res } = await getToken(query);
+      if (res) {
+        setCookie('user', res.token, { path: '/' });
+      }
+    };
+    setToken();
+  }, [query]);
+
+  useEffect(() => {
+    // cookieにuserがある場合rootへリダイレクト
+    if (cookies['user']) {
+      history.push('/');
+    }
+  }, [cookies]);
+
   return (
     <div>
       <p>hogehgoe</p>
