78e4674f2d74867f6cb8f0bd578f36a74661b5fa
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-17 12:27:42 +0900

    feat: change CookiesProvider to outside

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,18 +4,18 @@ import { history } from '../lib/plugins/history';
 import { Routes } from '../lib/routes';
 import { TopNavi } from './template/TopNavi';
 import { MenuNavi } from './template/MenuNavi';
-import { UserProvider } from '../lib/context/UserContext';
+import { CookiesProvider } from 'react-cookie';
 
 export const App: React.FC = () => {
   return (
     <>
-      <Router history={history}>
-        <UserProvider>
+      <CookiesProvider>
+        <Router history={history}>
           <TopNavi />
           <MenuNavi />
           <Routes />
-        </UserProvider>
-      </Router>
+        </Router>
+      </CookiesProvider>
     </>
   );
 };
--- src/lib/context/UserContext.tsx
+++ src/lib/context/UserContext.tsx
@@ -1,51 +1,42 @@
-import React, { createContext, useContext, FC, useMemo, useState, useEffect } from 'react';
-import { getToken } from '../api/auth';
+// import React, { createContext, useContext, FC, useMemo, useState, useEffect } from 'react';
+// import { getToken } from '../api/auth';
 
-interface UserState {
-  token: string | null;
-  signOut: () => void;
-}
+// interface UserState {
+//   token: string | null;
+//   signOut: () => void;
+// }
 
-export const useUserContext = () => useContext(useUserContext.context);
-useUserContext.context = createContext<UserState>({ token: null, signOut: () => {} });
+// export const useUserContext = () => useContext(useUserContext.context);
+// useUserContext.context = createContext<UserState>({ token: null, signOut: () => {} });
 
-export const UserProvider: FC = ({ children }) => {
-  const [token, setToken] = useState<string | null>(null);
-  // const userCookie = useMemo(() => {
-  //   const cookies = document.cookie.split('; ');
-  //   const cookie = cookies.find((cookie) => {
-  //     return cookie.startsWith('user');
-  //   });
-  //   if (cookie == undefined) return undefined;
-  //   return cookie;
-  // }, [token]);
-  // const [cookie] = useState(userCookie);
+// export const UserProvider: FC = ({ children }) => {
+//   const [token, setToken] = useState<string | null>(null);
 
-  const signOut = () => {
-    setToken(null);
-    document.cookie = `user=;max-age=0`;
-  };
+//   const signOut = () => {
+//     setToken(null);
+//     document.cookie = `user=;max-age=0`;
+//   };
 
-  const locationSearch = useMemo(() => {
-    return location.search;
-  }, [location.search]);
+//   const locationSearch = useMemo(() => {
+//     return location.search;
+//   }, [location.search]);
 
-  useEffect(() => {
-    if (locationSearch) {
-      const setTokenFunc = async () => {
-        const { res } = await getToken(locationSearch);
-        if (res) {
-          setToken(res.token);
-          document.cookie = `user=${res.token}`;
-        }
-      };
-      setTokenFunc();
-    }
-  }, [locationSearch]);
+//   useEffect(() => {
+//     if (locationSearch) {
+//       const setTokenFunc = async () => {
+//         const { res } = await getToken(locationSearch);
+//         if (res) {
+//           setToken(res.token);
+//           document.cookie = `user=${res.token}`;
+//         }
+//       };
+//       setTokenFunc();
+//     }
+//   }, [locationSearch]);
 
-  const value = useMemo<UserState>(() => {
-    return { token, signOut };
-  }, [token]);
+//   const value = useMemo<UserState>(() => {
+//     return { token, signOut };
+//   }, [token]);
 
-  return <useUserContext.context.Provider value={value} children={children} />;
-};
+//   return <useUserContext.context.Provider value={value} children={children} />;
+// };
