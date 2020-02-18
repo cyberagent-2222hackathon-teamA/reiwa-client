9705199d6310430939d738e37297d3b05de3e0bb
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-18 18:30:53 +0900

    feat: add timeline

--- src/components/page/Top.tsx
+++ src/components/page/Top.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 interface Props {
-  res: User;
+  timeLine: TimeLine;
 }
 
-const TopComp: React.FC<Props> = ({ res }) => {
+export const TopComp: React.FC<Props> = ({ timeLine }) => {
   return (
-    <>
-      <div>
-        <Link to={`/user/${res.id}`}>{res.name}</Link>
-        <p>{res.channel_id}</p>
-      </div>
-    </>
+    <div>
+      {timeLine.contributes.map((contributes: TimeLineContributes) => {
+        return (
+          <React.Fragment key={contributes.date + contributes.reaction_count + contributes.post_count}>
+            <div>
+              <div>
+                {/* <a href={`https://twitter.com/${contributes.user.name}`}></a> */}
+                <img src={contributes.user.profile_image_url} alt={contributes.user.name} />
+              </div>
+              <div>
+                <Link to={`/user/${contributes.user.name}`}>@{contributes.user.name}</Link>
+                <p>
+                  <span>{contributes.date}</span>
+                  <span>投稿数: {contributes.post_count}</span>
+                  <span>リアクション数: {contributes.reaction_count}</span>
+                </p>
+              </div>
+            </div>
+          </React.Fragment>
+        );
+      })}
+    </div>
   );
 };
-
-export default TopComp;
