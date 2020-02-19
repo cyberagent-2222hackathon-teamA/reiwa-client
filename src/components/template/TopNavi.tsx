99db99c02c5e975b379bffd7a6c8816b09bf808c
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-20 03:36:56 +0900

    feat: add Container style

--- src/components/template/TopNavi.tsx
+++ src/components/template/TopNavi.tsx
@@ -1,14 +1,27 @@
 import React, { useCallback } from 'react';
 import { history } from '../../lib/plugins/history';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  padding: 10px;
+  background-color: #24292e;
+  z-index: 1;
+  h1 {
+    cursor: pointer;
+    line-height: 50px;
+    color: white;
+  }
+`;
 
 export const TopNavi: React.FC = () => {
   const handleClickTitle = useCallback(() => {
     history.push('/');
   }, []);
   return (
-    <div>
+    <Container>
       <h1 onClick={handleClickTitle}>0+</h1>
-    </div>
+    </Container>
   );
 };
 
