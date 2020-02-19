5b28142791001f57da01b306c4f98175e6a98cc2
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-20 00:11:24 +0900

    feat: combine _fetch functions

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,21 +1,11 @@
-const apiBase = 'https://api.reiwa.cactus.click/v1/twitter';
-
-async function _fetch<T>(URL: string): Promise<{ res: Response | null; json: T | null; error: Error | null }> {
-  try {
-    const res = await fetch(URL);
-    const json = await res.json();
-    return { error: null, res, json };
-  } catch (error) {
-    return { error, res: null, json: null };
-  }
-}
+import { apiBase, _fetch } from './api';
 
 export const getTwitterURL = async () => {
-  const url = `${apiBase}/login`;
+  const url = `${apiBase}/twitter/login`;
   return _fetch<URL>(url);
 };
 
 export const getToken = async (query: string) => {
-  const url = `${apiBase}/callback${query}`;
+  const url = `${apiBase}/twitter/callback${query}`;
   return _fetch<Token>(url);
 };
