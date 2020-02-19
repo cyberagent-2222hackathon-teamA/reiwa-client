ad0503634321472ad0dd58d3d5478eb5d1214f8d
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-19 15:24:16 +0900

    feat: add react-calendar-heatmap

--- src/components/page/User.tsx
+++ src/components/page/User.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
+import CalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+import './reactCalendarHeatmap.css';
+import styled from 'styled-components';
 
 interface Props {
   user: User;
 }
 
+const Contribute = styled.div`
+  width: 100px;
+`;
+
 export const UserComp: React.FC<Props> = ({ user }) => {
+  const start = user.contributes[0].date;
+  const end = user.contributes[user.contributes.length - 1].date;
+
   return (
     <>
       <div>
         <p>{user.name}</p>
         <img src={user.twitter_profile_image} alt={user.name} />
       </div>
-      {user.contributes.map((contribute: UserContributes) => {
-        return (
-          <React.Fragment key={contribute.id}>
-            <p>{contribute.date}</p>
-            <p>{contribute.post_count}</p>
-            <p>{contribute.reaction_count}</p>
-          </React.Fragment>
-        );
-      })}
+      <Contribute>
+        <CalendarHeatmap
+          startDate={start}
+          endDate={end}
+          values={user.contributes.map((contribute: UserContributes) => {
+            return (
+              <React.Fragment key={contribute.id}>
+                [{`date: ${contribute.date}, count: ${contribute.post_count}`}]
+              </React.Fragment>
+            );
+          })}
+        />
+      </Contribute>
     </>
   );
 };
