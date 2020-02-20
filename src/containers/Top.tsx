b32597e40052ccb8286654c41f8b7b41a492475b
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-21 01:43:13 +0900

    feat: add private timeline

--- src/containers/Top.tsx
+++ src/containers/Top.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, useCallback, useMemo } from 'react';
 import { useRouteMatch } from 'react-router';
+import { useCookies } from 'react-cookie';
 import { getTimeLine } from '../lib/api/timeline';
 import { TopComp } from '../components/pages/Top';
 
 const Top: React.FC = () => {
+  const [cookies] = useCookies(['user']);
   const [timeLine, setTimeLine] = useState<TimeLine | null>(null);
   const matchPageNumber = useRouteMatch<{ pageNumber: string }>({
     path: '/page=:pageNumber',
@@ -25,6 +27,15 @@ const Top: React.FC = () => {
     if (res) setTimeLine(res);
   }, [pageNumber]);
 
+  const getLoginTimeLineData = useCallback(async () => {
+    const res = await fetch('https://api.reiwa.cactus.click/v1/timeline/private', {
+      headers: new Headers({
+        Authorization: cookies.user,
+      }),
+    });
+    console.log(await res.json());
+  }, [pageNumber]);
+
   return (
     <>
       {timeLine && (
@@ -32,6 +43,7 @@ const Top: React.FC = () => {
           <TopComp timeLine={timeLine} />
         </div>
       )}
+      <button onClick={getLoginTimeLineData}>getLoginTimeLineData</button>
     </>
   );
 };
