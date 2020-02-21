3e2dfdb18d5444f3ba744f8e7d8a0a37bf40f32f
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-21 16:49:46 +0900

    feat: add useRef sample

--- src/components/pages/Top/index.tsx
+++ src/components/pages/Top/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useRef, useEffect } from 'react';
 import { Pager } from '../../navigation/pager';
 import { TimeLineCard } from './internal/TimelineCard';
 
@@ -14,10 +14,30 @@ export const TopComp: React.FC<Props> = ({ timeLine }) => {
     }
   }, [timeLine]);
 
+  const lazyLoadArea = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const options: IntersectionObserverInit = {
+      root: lazyLoadArea.current,
+      rootMargin: '0px',
+      threshold: 1,
+    };
+
+    const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach((entrie) => {
+        // eslint-disable-next-line no-console
+        console.log(entrie, observer);
+      });
+    };
+    const observer = new IntersectionObserver(callback, options);
+    // eslint-disable-next-line no-console
+    console.log({ observer, options });
+  }, []);
+
   return (
     <div>
       {pager && <Pager pager={pager} />}
-      <div>
+      <div ref={lazyLoadArea}>
         {timeLine.contributes.map((contributes: TimeLineContributes) => {
           return <TimeLineCard key={contributes.id} contributes={contributes}></TimeLineCard>;
         })}
