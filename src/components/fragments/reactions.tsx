927a9019ff2f5c12d12f557537b844d249f8b529
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-22 02:06:48 +0900

    feat: rename to StampContainer

--- src/components/fragments/reactions.tsx
+++ src/components/fragments/reactions.tsx
@@ -5,15 +5,15 @@ interface Props {
   reactions: Stamp;
 }
 
-const StampArea = styled.div`
+const StampContainer = styled.div`
   display: flex;
 `;
 
 export const Reactions: React.FC<Props> = ({ reactions }) => {
   return (
-    <StampArea>
+    <StampContainer>
       <p>スタンプ名{reactions.name}</p>
       <p>スタンプ数{reactions.count}</p>
-    </StampArea>
+    </StampContainer>
   );
 };
